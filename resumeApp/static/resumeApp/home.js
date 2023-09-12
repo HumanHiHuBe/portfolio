3c6c9962a9e458a4ade2a98723aa2be72a1d1b9f
vatsa <vatsanatural@outlook.com>
2023-09-12 10:44:09 +0530

    before final but everything working

--- resumeApp/static/resumeApp/home.js
+++ resumeApp/static/resumeApp/home.js
@@ -17,16 +17,35 @@ function f2(spid) {
 
 function f3(bid) {
     ele = document.getElementsByTagName('section')
-    sValue = document.getElementById('i1').value
-    console.log(sValue)
+    let sValue = document.getElementById('i1').value
     let dict1 = new Map()
     for(i=0; i<ele.length; i++){
         dict1.set(ele[i], ele[i].textContent)
     }
     for(let [key,value] of dict1){
-        if(value.search(/sValue/i)){
-            console.log(sValue)
-        };
+        let newBol = "/"+sValue+"/"+"i"
+        // console.log(newBol)
+        // console.log(key.innerText)
+        // let res = key.innerText.search(newBol)
+        // console.log(res)
+        // if(res == -1){
+        //     console.log("Didn't find")
+        // }else{
+        //     console.log(sValue)
+        //     key.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" })
+        //     console.log(key)
+        //     console.log(value)
+        //     break
+        // }
+        
+
+        // if(key.innerText.search(newBol)){
+        //     key.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" })
+        // }
+        
+
+
+
     }
 
 }
