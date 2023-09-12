ae7d7cff7e6926def94a289974bfb21bceda26c4
vatsa <vatsanatural@outlook.com>
2023-09-12 08:13:07 +0530

    Commiting after Search functinality in middle

--- resumeApp/static/resumeApp/home.js
+++ resumeApp/static/resumeApp/home.js
@@ -10,9 +10,23 @@ function f1(inid) {
 function f2(spid) {
     ele = document.getElementById(spid)
     ele.click(function f() {
-
-        window.top.close();
-        
+        window.top.close();   
     })
     //working
+}
+
+function f3(bid) {
+    ele = document.getElementsByTagName('section')
+    sValue = document.getElementById('i1').value
+    console.log(sValue)
+    let dict1 = new Map()
+    for(i=0; i<ele.length; i++){
+        dict1.set(ele[i], ele[i].textContent)
+    }
+    for(let [key,value] of dict1){
+        if(value.search(/sValue/i)){
+            console.log(sValue)
+        };
+    }
+
 }
